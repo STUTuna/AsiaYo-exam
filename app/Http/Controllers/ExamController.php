3454b4685e9a1ae3b748cc423a63ef0ad44451e5
STUTuna <df95111111@gmail.com>
2023-10-18 20:47:06 +0800

    feat(ExamController): 接上匯率換算功能

--- app/Http/Controllers/ExamController.php
+++ app/Http/Controllers/ExamController.php
@@ -3,16 +3,44 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\CurrencyExchangeRequest;
+use App\Services\CurrencyExchangeService;
 
 class ExamController extends Controller
 {
-    // 匯率換算
+
+    private $currencyExchangeService;
+    public function __construct(CurrencyExchangeService $currencyExchangeService)
+    {
+        // 注入CurrencyExchangeService
+        $this->currencyExchangeService = $currencyExchangeService;
+    }
+    /**
+     * 匯率換算
+     * @param CurrencyExchangeRequest $request
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function currencyExchange(CurrencyExchangeRequest $request)
     {
         // 用formRequest進行驗證
-        $request->validated();
+        $validated = $request->validated();
+
+        try {
+            // 呼叫CurrencyExchangeService的convert方法進行匯率換算
+            $exchangedAmount = $this->currencyExchangeService->convert(
+                $validated['source'],
+                $validated['target'],
+                $validated['amount']
+            );
+        } catch (\Exception $e) {
+            return response()->json([
+                'msg' => 'fail',
+                'amount' => $e->getMessage(),
+            ]);
+        }
+
         return response()->json([
-            'message' => 'success',
+            'msg' => 'success',
+            'amount' => $exchangedAmount,
         ]);
     }
 }
