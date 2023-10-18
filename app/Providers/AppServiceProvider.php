73880432f09224d603c758317d7cc848ae90a69a
STUTuna <df95111111@gmail.com>
2023-10-18 19:01:45 +0800

    feat: 將匯率依賴注入

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,7 +11,28 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(CurrencyExchangeService::class, function ($app) {
+            return new CurrencyExchangeService([
+                'currencies'=> [
+                    'TWD'=> [
+                        'TWD'=> 1,
+                        'JPY'=> 3.669,
+                        'USD'=> 0.03281,
+                    ],
+                    'JPY'=> [
+                        'TWD'=> 0.26956,
+                        'JPY'=> 1,
+                        'USD'=> 0.00885,
+                    ],
+                    'USD'=> [
+                        'TWD'=> 30.444,
+                        'JPY'=> 111.801,
+                        'USD'=> 1,
+                    ],
+                ],
+            ])
+
+        });
     }
 
     /**
