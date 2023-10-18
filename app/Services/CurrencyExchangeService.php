58f6018658174e9001b58590c09ffe9023ae7774
STUTuna <df95111111@gmail.com>
2023-10-18 19:27:04 +0800

    feat(CurrencyExchangeService): 新增匯率轉換功能及相關註解

--- app/Services/CurrencyExchangeService.php
+++ app/Services/CurrencyExchangeService.php
@@ -3,5 +3,41 @@ namespace App\Services;
 
 class CurrencyExchangeService
 {
+    // 匯率表
+    private $currencies;
+
+    /**
+     * CurrencyExchangeService constructor.
+     * @param array $currencies 匯率表
+     */
+    public function __construct(array $currencies)
+    {
+        // 將匯率表存入此service變數
+        $this->currencies = $currencies;
+    }
+
+    /**
+     * 轉換匯率
+     * @param string $source 來源幣別
+     * @param string $target 目標幣別
+     * @param float $amount 金額
+     * @return string 轉換後的金額
+     */
+    public function convert($source, $target, $amount)
+    {
+        // 檢查來源幣別與目標幣別是否存在
+        if (!isset($this->currencies[$source]) || !isset($this->currencies[$source][$target])) {
+            throw new \Exception('未定義的幣別，無法轉換');
+        }
+
+        // 移除千分位，並轉換成float
+        $amount = floatval(str_replace(',', '', $amount));
+
+        // 匯率換算 = 金額 * 匯率
+        $convertedAmount = $amount * $this->currencies[$source][$target];
+
+        // 回傳轉換後的金額(1. 四捨五入到小數點後兩位 2. 加上半形逗點作為千分位表示，每三個位數一點)
+        return number_format($convertedAmount, 2);
+    }
 
 }
